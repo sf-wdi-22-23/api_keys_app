ab91f86957f76cff5ea7762aa3b737c861266324
Juliana <jlopker@gmail.com>
2015-10-19 18:30:39 -0700

    template

--- server.js
+++ server.js
@@ -4,10 +4,7 @@
 var express = require("express"),
     app = express(),
     path = require("path"),
-    bodyParser = require("body-parser"),
-    request = require('request');
-
-require('dotenv').load();
+    bodyParser = require("body-parser");
 
 // CONFIG //
 // set ejs as view engine
@@ -17,18 +14,8 @@ app.use("/static", express.static("public"));
 // body parser config to accept our datatypes
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var FOOD_API_KEY = process.env.FOOD_API_KEY;
-
-var foods;
-
-request('http://food2fork.com/api/search?key='+FOOD_API_KEY+'&q=chocolate', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    foods = JSON.parse(body).recipes;
-  }
-});
-
 app.get('/', function(req, res) {
-    res.render('index', {foods: foods});
+    res.render('index');
 });
 
 app.listen(3000, function (){
