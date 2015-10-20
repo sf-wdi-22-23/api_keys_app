a84e5047533d9f91e01a53a0c95e335ed0cc7408
Juliana <jlopker@gmail.com>
2015-10-19 17:38:29 -0700

    food2fork api

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,5 @@
 // CLIENT-SIDE JAVASCRIPT
 // On page load
 $(document).ready(function(){
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 37.773972, lng: -122.431297},
-    zoom: 10
-  });
+    
 });
--- server.js
+++ server.js
@@ -4,7 +4,8 @@
 var express = require("express"),
     app = express(),
     path = require("path"),
-    bodyParser = require("body-parser");
+    bodyParser = require("body-parser"),
+    request = require('request');
 
 require('dotenv').load();
 
@@ -16,10 +17,18 @@ app.use("/static", express.static("public"));
 // body parser config to accept our datatypes
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var GOOGLE_MAPS_API_KEY = process.env.GOOGLE_MAPS_API_KEY;
+var FOOD_API_KEY = process.env.FOOD_API_KEY;
+
+var foods;
+
+request('http://food2fork.com/api/search?key='+FOOD_API_KEY+'&q=chocolate', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    foods = JSON.parse(body).recipes;
+  }
+});
 
 app.get('/', function(req, res) {
-    res.render('index', {GOOGLE_MAPS_API_KEY: GOOGLE_MAPS_API_KEY});
+    res.render('index', {foods: foods});
 });
 
 app.listen(3000, function (){
